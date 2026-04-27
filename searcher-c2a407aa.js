04369c0358b38a03cfd3b4f370dea3568d55e8cb
degen-pool <dead.pool.aka.wilson@gmail.com>
2026-04-27 19:49:30 +0900

    Publish docs from dead-pool-aka-wilson/apyx-sdk@e8ebb24

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-210f62b5.js',
+                path_to_root + 'searchindex-a86c4a1c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
